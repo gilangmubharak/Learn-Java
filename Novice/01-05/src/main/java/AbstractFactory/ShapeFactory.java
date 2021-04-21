9ac9970ceef22e94cc87a8a30cb8397385ca3383
gilangmubharak <gilang1700016106@webmail.uad.ac.id>
2021-04-21 16:16:32 +0700

    Day 5

---
+++ Novice/01-05/src/main/java/AbstractFactory/ShapeFactory.java
@@ -0,0 +1,16 @@
+package AbstractFactory;
+
+import FactoryPattern.Square;
+
+public class ShapeFactory extends AbstractFactory {
+    @Override
+    public Shape getShape(String shapeType){
+        if(shapeType.equalsIgnoreCase("RECTANGLE")){
+            return new Rectangle();
+        }
+        else if(shapeType.equalsIgnoreCase("SQUARE")){
+            return new RoundedSquare();
+        }
+        return null;
+    }
+}
---
+++ Novice/01-05/src/main/java/FactoryPattern/ShapeFactory.java
@@ -0,0 +1,18 @@
+package FactoryPattern;
+
+public class ShapeFactory {
+    //use get Shape method to get object of type shape
+    public Shape getShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+        }
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+            return new Rectangle();
+        } else if (shapeType.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        }
+        return null;
+    }
+}
